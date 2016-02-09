60ce5373703e61333f8381db0a7ea04f1f9702a3
Bill DeCoste <wdecoste@redhat.com>
2016-02-09 11:40:02 -0500

    use real IP instead of localhost
    cert validation off

--- app-jee/src/main/java/org/keycloak/quickstart/appjee/ServiceClient.java
+++ app-jee/src/main/java/org/keycloak/quickstart/appjee/ServiceClient.java
@@ -18,13 +18,28 @@ package org.keycloak.quickstart.appjee;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.AdapterUtils;
 import org.keycloak.util.JsonSerialization;
@@ -71,44 +86,88 @@ public class ServiceClient {
     }
 
     private static String getServiceUrl(HttpServletRequest req, KeycloakSecurityContext session) {
-        String uri = req.getServletContext().getInitParameter(SERVICE_URI_INIT_PARAM_NAME);
-        if (uri != null) return uri;
+//        String uri = req.getServletContext().getInitParameter(SERVICE_URI_INIT_PARAM_NAME);
+//        if (uri != null) return uri;
 
-        uri = AdapterUtils.getOriginForRestCalls(req.getRequestURL().toString(), session);
+        String uri = AdapterUtils.getOriginForRestCalls(req.getRequestURL().toString(), session);
         if (uri != null) return uri + "/service";
 
-        return "http://localhost:8080/service";
+        String ip = "localhost";
+        try {
+	    ip = java.net.InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "http://" + ip + ":8080/service";
+    }
+    
+    public static HttpClient createHttpClient_AcceptsUntrustedCerts() throws Exception {
+        HttpClientBuilder b = HttpClientBuilder.create();
+     
+        // setup a Trust Strategy that allows all certificates.
+        //
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                return true;
+            }
+        }).build();
+        b.setSslcontext( sslContext);
+     
+        // don't check Hostnames, either.
+        //      -- use SSLConnectionSocketFactory.getDefaultHostnameVerifier(), if you don't want to weaken
+        HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+     
+        // here's the special part:
+        //      -- need to create an SSL Socket Factory, to use our weakened "trust strategy";
+        //      -- and create a Registry, to register it.
+        //
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+     
+        // now, we create connection-manager using our Registry.
+        //      -- allows multi-threaded use
+        PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager( socketFactoryRegistry);
+        b.setConnectionManager( connMgr);
+     
+        // finally, build the HttpClient;
+        //      -- done!
+        HttpClient client = b.build();
+        return client;
     }
 
     public static String callService(HttpServletRequest req, KeycloakSecurityContext session, String action) throws Failure {
-        HttpClient client = new DefaultHttpClient();
+        HttpClient client = null;
         try {
+        	client = createHttpClient_AcceptsUntrustedCerts();
             HttpGet get = new HttpGet(getServiceUrl(req, session) + "/" + action);
             if (session != null) {
                 get.addHeader("Authorization", "Bearer " + session.getTokenString());
             }
 
-            try {
-                HttpResponse response = client.execute(get);
+            HttpResponse response = client.execute(get);
 
-                StatusLine status = response.getStatusLine();
-                if (status.getStatusCode() != 200) {
-                    throw new Failure(status.getStatusCode(), status.getReasonPhrase());
-                }
-
-                HttpEntity entity = response.getEntity();
-                InputStream is = entity.getContent();
-                try {
-                    MessageBean message = (MessageBean)JsonSerialization.readValue(is, MessageBean.class);
-                    return message.getMessage();
-                } finally {
-                    is.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() != 200) {
+                throw new Failure(status.getStatusCode(), status.getReasonPhrase());
             }
+
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            try {
+                MessageBean message = (MessageBean)JsonSerialization.readValue(is, MessageBean.class);
+                return message.getMessage();
+            } finally {
+                is.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         } finally {
-            client.getConnectionManager().shutdown();
+        	if (client != null)
+             client.getConnectionManager().shutdown();
         }
     }
 }
