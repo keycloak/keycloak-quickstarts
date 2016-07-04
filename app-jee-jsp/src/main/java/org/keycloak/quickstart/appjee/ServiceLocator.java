936260015dcb0e52132931355054942f041a3482
Bill DeCoste <wdecoste@redhat.com>
2016-07-04 12:31:15 -0400

    enable for ose

--- app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
+++ app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
@@ -8,33 +8,40 @@ import java.net.URL;
  * @author <a href="mailto:mstrukel@redhat.com">Marko Strukelj</a>
  */
 public class ServiceLocator {
+	
+	private static final String SERVICE_URI_INIT_PARAM_NAME = "serviceUrl";
 
-    public static URL getServiceUrl(HttpServletRequest req) {
-
-        String uri = null;
-        try {
-            uri = System.getProperty("service.url");
-            if (uri != null) {
-                return new URL(uri);
-            }
-
-            uri = System.getenv("SERVICE_URL");
-            if (uri != null) {
-                return new URL(uri);
-            }
-
-            String host = new URL(req.getRequestURL().toString()).getHost();
-            if (host == null) {
-                host = "localhost";
-            }
-
-            uri = "http://" + host + ":8080/service";
-            return new URL(uri);
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Malformed url: " + uri);
-        } finally {
-            System.out.println("Service url: " + uri);
-        }
-    }
+	public static URL getServiceUrl(HttpServletRequest req) {
+		URL serviceUrl = null;
+		
+		try {
+			String url = req.getServletContext().getInitParameter(SERVICE_URI_INIT_PARAM_NAME);
+		    if (url != null && !url.contains("localhost")){
+		    	try {
+					serviceUrl = new URL(url);
+					return serviceUrl;
+				} catch (MalformedURLException e){
+					throw new RuntimeException("Malformed URL: " + url);
+				}
+		    }
+	
+			String host = req.getLocalAddr();
+	
+			if (host.equals("localhost")) {
+				try {
+					host = java.net.InetAddress.getLocalHost().getHostAddress();
+				} catch (Exception e) {
+				}
+			}
+			
+			try {
+				serviceUrl = new URL("http://" + host + ":8080/service");
+				return serviceUrl;
+			} catch (MalformedURLException e){
+				throw new RuntimeException("Malformed URL: " + host);
+			}
+		} finally {
+			System.out.println("Using Service URL " + serviceUrl);
+		}
+	}
 }
