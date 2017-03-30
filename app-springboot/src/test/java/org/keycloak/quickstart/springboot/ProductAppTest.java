cae637c3250c011351ff40cd0ed4d2d05b833d1d
sebastienblanc <scm.blanc@gmail.com>
2017-03-30 11:26:58 +0200

    updated readme and small polishing

---
+++ app-springboot/src/test/java/org/keycloak/quickstart/springboot/ProductAppTest.java
@@ -0,0 +1,91 @@
+package org.keycloak.quickstart.springboot;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.helper.TestsHelper;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by sblanc on 3/28/17.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT, classes = {ProductApplication.class})
+public class ProductAppTest {
+
+    //@Autowired
+    private WebClient webClient = new WebClient(BrowserVersion.CHROME);
+
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        TestsHelper.baseUrl = "http://localhost:8080";
+        //TestsHelper.keycloakBaseUrl  = "set keycloak server docker IP"
+        TestsHelper.testRealm="quickstart";
+        TestsHelper.initialAccessTokenCount = 3;
+        TestsHelper.ImportTestRealm("admin","admin","/quickstart-realm.json");
+        TestsHelper.createDirectGrantClient();
+        TestsHelper.createClient(generateClientRepresentation());
+        TestsHelper.createClient(generateServiceClientRepresentation());
+
+    }
+
+    @Test
+    public void testRedirect() throws IOException {
+        HtmlPage page = this.webClient.getPage("http://localhost:8080/products");
+        Assert.assertTrue(page.getBody().getTextContent().contains("Username or email"));
+    }
+
+    @Test
+    public void testLogin() throws IOException {
+        HtmlPage page = this.webClient.getPage("http://localhost:8080/products");
+        ((HtmlInput)page.getElementById("username")).setValueAttribute("alice");
+        ((HtmlInput)page.getElementById("password")).setValueAttribute("password");
+        HtmlPage protectedPage = page.getElementByName("login").click();
+        Assert.assertTrue(protectedPage.getTitleText().contains("Product Page"));
+    }
+
+    @Test
+    public void testLogout() throws IOException {
+        HtmlPage page = this.webClient.getPage("http://localhost:8080/products");
+        ((HtmlInput)page.getElementById("username")).setValueAttribute("alice");
+        ((HtmlInput)page.getElementById("password")).setValueAttribute("password");
+        HtmlPage protectedPage = page.getElementByName("login").click();
+        HtmlPage landingPage = protectedPage.getElementById("logout").click();
+        Assert.assertTrue(landingPage.getTitleText().contains("Landing Page"));
+    }
+
+
+    private static ClientRepresentation generateClientRepresentation() {
+        ClientRepresentation clientRepresentation = new ClientRepresentation();
+        clientRepresentation.setClientId("test-demo");
+        clientRepresentation.setPublicClient(true);
+        clientRepresentation.setBaseUrl(TestsHelper.baseUrl);
+        clientRepresentation.setRedirectUris(Arrays.asList("http://localhost:8080/*"));
+        return clientRepresentation;
+    }
+
+    private static ClientRepresentation generateServiceClientRepresentation() {
+        ClientRepresentation clientRepresentation = new ClientRepresentation();
+        clientRepresentation.setClientId("product-service");
+        clientRepresentation.setBearerOnly(true);
+        return clientRepresentation;
+    }
+
+    @AfterClass
+    public static void cleanUp() throws IOException{
+        TestsHelper.deleteRealm("admin","admin",TestsHelper.testRealm);
+    }
+}
---
+++ service-springboot-rest/src/test/java/org/keycloak/quickstart/springboot/ProductServiceTest.java
@@ -0,0 +1,66 @@
+package org.keycloak.quickstart.springboot;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.helper.TestsHelper;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+/**
+ * Created by sblanc on 3/28/17.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT, classes = {ProductServiceApplication.class})
+
+public class ProductServiceTest {
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        TestsHelper.baseUrl = "http://localhost:8081";
+        //TestsHelper.keycloakBaseUrl  = "set keycloak server docker IP"
+        TestsHelper.testRealm="quickstart";
+        TestsHelper.ImportTestRealm("admin","admin","/quickstart-realm.json");
+        TestsHelper.createDirectGrantClient();
+        TestsHelper.createClient(generateClientRepresentation());
+
+    }
+
+    @Test
+    public void testUnauthenticatedSecuredEndpoint()  {
+        try {
+            Assert.assertTrue(TestsHelper.returnsForbidden("/products"));
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testAuthenticatedSecuredEndpoint()  {
+
+        try {
+            Assert.assertTrue(TestsHelper.testGetWithAuth("/products", TestsHelper.getToken("alice","password",TestsHelper.testRealm)));
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    private static ClientRepresentation generateClientRepresentation() {
+        ClientRepresentation clientRepresentation = new ClientRepresentation();
+        clientRepresentation.setClientId("test-demo");
+        clientRepresentation.setBaseUrl(TestsHelper.baseUrl);
+        clientRepresentation.setBearerOnly(true);
+        return clientRepresentation;
+    }
+
+    @AfterClass
+    public static void cleanUp() throws IOException{
+        TestsHelper.deleteRealm("admin","admin",TestsHelper.testRealm);
+    }
+}
