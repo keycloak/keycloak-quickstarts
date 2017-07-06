74621f7f6f3c74f4b1438349a0e13dafbbd2349c
sebastienblanc <scm.blanc@gmail.com>
2017-07-06 15:18:05 +0200

    new profile for bump version

--- set-version.sh
+++ set-version.sh
@@ -2,5 +2,5 @@
 
 NEW_VERSION=$1
 
-mvn versions:update-parent -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false
-mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
+mvn versions:update-parent -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
+mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
