1251419f821c3e7530523d3f1ccdf974fdf3ee82
sebastienblanc <scm.blanc@gmail.com>
2017-08-10 14:46:53 +0200

    Remove not supported quickstarts

--- productize.sh
+++ productize.sh
@@ -2,6 +2,7 @@
 git config user.name "${GH_USER_NAME}"
 git config user.email "{GH_USER_EMAIL}"
 
+
 # Rename Keycloak to Red Hat SSO
 find . -type f -name "*README*" -exec sed -i 's@<span>Keycloak</span>@Red Hat SSO@g' {} +
 # Rename WildFly to JBoss EAP
@@ -21,6 +22,9 @@ find . -type f -name "*README*" -exec sed -i 's@WILDFLY_HOME\bin@EAP_HOME\bin@g'
 #TODO rename rename groupId in POMs
 
 git checkout -b prod_staging
+git rm -r action-token-authenticator
+git rm -r action-token-required-action
 git status
+
 git commit . -m "rename pom and readme"
 git push --force "https://${GH_TOKEN}@${GH_REF}" prod_staging:production-test
