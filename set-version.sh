c836c30b3fd1696a05765e58317e3cc4f02cb032
Stian Thorgersen <stianst@gmail.com>
2017-11-08 19:48:38 +0100

    Fix issues with bumping versions on release

--- set-version.sh
+++ set-version.sh
@@ -2,5 +2,5 @@
 
 NEW_VERSION=$1
 
-mvn versions:update-parent -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
-mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
+mvn versions:update-parent -Dversion.keycloak=$NEW_VERSION -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
+mvn versions:set -Dversion.keycloak=$NEW_VERSION -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
