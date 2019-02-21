010c73220240e18492b84685a807d35db3d3cc2d
stianst <stianst@gmail.com>
2019-02-21 09:57:59 +0100

    Set version 5.0.0

--- set-version.sh
+++ set-version.sh
@@ -2,5 +2,7 @@
 
 NEW_VERSION=$1
 
-mvn versions:update-parent -Dversion.keycloak=$NEW_VERSION -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
-mvn versions:set -Dversion.keycloak=$NEW_VERSION -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
+LATEST_KEYCLOAK_VERSION=`curl -s "http://search.maven.org/solrsearch/select?q=g:org.keycloak%20AND%20a:keycloak-parent&rows=1&wt=json" | jq -r .response.docs[0].latestVersion`
+
+mvn versions:update-parent -Dversion.keycloak=$LATEST_KEYCLOAK_VERSION -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
+mvn versions:set -Dversion.keycloak=$LATEST_KEYCLOAK_VERSION -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
