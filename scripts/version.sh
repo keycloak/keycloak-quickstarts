866695c4fa899cdd841f77ae95fd083868d1e4a2
Denis <drichtar@redhat.com>
2020-01-27 15:49:48 +0100

    KEYCLOAK-12676 Shell script from the Quickstarts return an empty version of Keycloak

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
+export VERSION=$(curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -e '[0-9]\{1,2\}\.[0-9]\.[0-9]/' | cut -d '/' -f1 | tail -n1)
 export KEYCLOAK="keycloak-${VERSION}"
