fdf67cced76a8b716bd017a1ec1da29241952ebb
stianst <stianst@gmail.com>
2020-03-30 19:47:47 +0200

    Update Kubernetes and OpenShift versions

--- set-version.sh
+++ set-version.sh
@@ -7,5 +7,5 @@ LATEST_KEYCLOAK_VERSION=`curl -s "http://search.maven.org/solrsearch/select?q=g:
 mvn versions:update-parent -Dversion.keycloak=$LATEST_KEYCLOAK_VERSION -DparentVersion=$NEW_VERSION -DgenerateBackupPoms=false -Pbump-version
 mvn versions:set -Dversion.keycloak=$LATEST_KEYCLOAK_VERSION -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=* -Pbump-version
 
-
 sed -i "s|keycloak/keycloak:.*|keycloak/keycloak:$NEW_VERSION|" kubernetes-examples/keycloak.yaml
+sed -i "s|keycloak/keycloak:.*|keycloak/keycloak:$NEW_VERSION|" openshift-examples/keycloak.yaml
