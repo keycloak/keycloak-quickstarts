445e41714d4f101faba4f055fc2774c27d5a2199
vmuzikar <vmuzikar@redhat.com>
2021-01-15 13:51:50 +0100

    KEYCLOAK-16154 Migrate QuickStarts to GitHub Actions

--- scripts/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
-export KEYCLOAK="keycloak-${VERSION}"
