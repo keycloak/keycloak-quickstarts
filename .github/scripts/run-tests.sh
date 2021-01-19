9aadd6fb98dbfc3e734e67c54c7ba9a31fa8f20f
vmuzikar <vmuzikar@redhat.com>
2021-01-19 16:49:17 +0100

    Add the rest of the tests

--- .github/scripts/run-tests.sh
+++ .github/scripts/run-tests.sh
@@ -1,33 +1,68 @@
 #!/bin/bash
+set -o pipefail
 
 mkdir test-logs
 
-set -x
-mvn clean test -Pwildfly-managed -f action-token-required-action -B 2>&1 | tee test-logs/action-token-required-action.log
-mvn clean test -Pwildfly-managed -f action-token-authenticator -B 2>&1 | tee test-logs/action-token-authenticator.log
-mvn clean test -Pwildfly-managed -f app-angular2 -B 2>&1 | tee test-logs/app-angular2.log
-mvn clean test -Pkeycloak-remote -f user-storage-simple -B 2>&1 | tee test-logs/user-storage-simple.log
-mvn clean test -Pkeycloak-remote -f user-storage-jpa -B 2>&1 | tee test-logs/user-storage-jpa.log
-set +x
+tests_with_errors=()
 
-testsWithErrors=()
-
-for log in test-logs/*.log; do
-  if grep -q "BUILD FAILURE" "$log"; then
-    fileName=$(echo "$log" | sed -r "s/.+\/(.+)\..+/\1/")
-    testsWithErrors+=("$fileName")
+run_tests() {
+  module=$1
+  args="${*:2}"
+  echo "Args: $args"
+  printf "\n\n\n*****************************************\n"
+  echo "Running tests for $module QS"
+  echo "*****************************************"
+  if ! mvn clean test -f $module $args -B 2>&1 | tee test-logs/$module.log; then
+    tests_with_errors+=("$module")
   fi
-done
+}
 
-if [ ${#testsWithErrors[@]} -eq 0 ]; then
-  echo "--------------------------------"
-  echo "NO TEST ERRORS FOUND"
-  echo "--------------------------------"
-else
-  echo "--------------------------------"
-  echo "FAILED TESTS:"
-  printf '%s\n' "${testsWithErrors[@]}"
-  echo "--------------------------------"
-  echo "Check individual logs for details"
-  exit 1
+print_failed_tests() {
+  printf "\n\n\n*****************************************\n\n\n"
+  if [ ${#tests_with_errors[@]} -eq 0 ]; then
+    echo "CONGRATS, NO TEST ERRORS FOUND"
+  else
+    echo "FAILED TESTS:"
+    printf -- '- %s\n' "${tests_with_errors[@]}"
+    echo "Check individual logs for details"
+    exit 1
+  fi
+}
+
+run_tests action-token-required-action -Pwildfly-managed
+run_tests action-token-authenticator -Pwildfly-managed
+run_tests app-angular2 -Pwildfly-managed
+run_tests app-authz-jee-servlet -Pwildfly-managed
+run_tests app-authz-jee-vanilla -Pwildfly-managed
+run_tests app-authz-photoz -Pwildfly-managed
+run_tests app-authz-rest-employee -Pwildfly-managed
+run_tests app-authz-rest-springboot -Pwildfly-managed
+run_tests app-authz-spring-security -Pwildfly-managed
+run_tests app-authz-springboot -Pspring-boot
+run_tests app-authz-uma-photoz -Pwildfly-managed
+run_tests app-jee-html5 -Pwildfly-managed
+run_tests app-jee-jsp -Pwildfly-managed
+run_tests app-profile-jee-html5 -Pwildfly-managed
+run_tests app-profile-jee-jsp -Pwildfly-managed
+run_tests app-profile-jee-vanilla -Pwildfly-managed
+run_tests app-profile-saml-jee-jsp -Pwildfly-managed
+run_tests event-listener-sysout -Pkeycloak-remote
+run_tests event-store-mem -Pkeycloak-remote
+run_tests extend-account-console -Pwildfly-managed
+run_tests fuse63
+run_tests fuse70
+run_tests service-jee-jaxrs -Pwildfly-managed
+run_tests service-springboot-rest -Pspring-boot
+run_tests user-storage-jpa -Pkeycloak-remote
+run_tests user-storage-simple -Pkeycloak-remote
+
+mvn -f service-springboot-rest spring-boot:run >/dev/null&
+run_tests app-springboot -Pspring-boot
+
+# service-nodejs test
+npm -C service-nodejs start >/dev/null&
+if ! npm -C service-nodejs test 2>&1 | tee test-logs/service-nodejs.log; then
+  tests_with_errors+=("service-nodejs")
 fi
+
+print_failed_tests
