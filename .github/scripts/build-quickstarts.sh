bd71f84b8e907587fe1cf87b916096229cfbafbe
Václav Muzikář <vmuzikar@redhat.com>
2021-04-13 17:26:51 +0200

    KEYCLOAK-17743 Run QS tests with each PR in the Keycloak repo

--- .github/scripts/build-quickstarts.sh
+++ .github/scripts/build-quickstarts.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
-## This is a helper script for generating "keycloak.json" files from "keycloak-example.json".
-## Use this to test your changes against the Enforcer Plugin.
+cp -f maven-settings.xml ~/.m2/settings.xml
 
+# generate keycloak.json
 for f in $(find . -type f -name 'keycloak-example.json'); do
    cp "$f" "${f%-example.json}.json"
 done
@@ -10,3 +10,6 @@ done
 for f in $(find . -type f -name 'keycloak-saml-example.xml'); do
    cp "$f" "${f%-example.xml}.xml"
 done
+
+mvn clean install -DskipTests -B
+cp authz-js-policies/target/authz-js-policies.jar keycloak-dist/standalone/deployments
