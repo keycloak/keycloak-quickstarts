d28eec44db8ed231cc631d43e418687f11b0627d
Lukas Hanusovsky <lhanusov@redhat.com>
2021-08-19 10:28:35 +0200

    KEYCLOAK-19110 -> KC version related to JS tests, Account management console test

--- app-profile-saml-jee-jsp/src/test/java/org/keycloak/quickstart/ArquillianProfileSamlJeeJspTest.java
+++ app-profile-saml-jee-jsp/src/test/java/org/keycloak/quickstart/ArquillianProfileSamlJeeJspTest.java
@@ -48,6 +48,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.keycloak.test.TestsHelper.createClient;
 import static org.keycloak.test.TestsHelper.deleteRealm;
@@ -140,7 +141,7 @@ public class ArquillianProfileSamlJeeJspTest {
             indexPage.clickLogin();
             loginPage.login("alice", "password");
             profilePage.clickAccount();
-            assertEquals("Keycloak Account Management", webDriver.getTitle());
+            assertTrue(webDriver.getTitle().contains("Account Management"));
         } catch (Exception e) {
             debugTest(e);
             fail("Should display account management page");
