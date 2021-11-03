bbf42c1db3b9025c52e81232696554a9be841e18
Bruno Oliveira da Silva <bruno@abstractj.org>
2021-11-03 12:06:11 -0300

    [KEYCLOAK-19756] - Update the quickstarts to remove problematic language in the repository

--- scripts/prepare-local-server.sh
+++ scripts/prepare-local-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-upstream_master() {
+upstream_main() {
   git clone https://github.com/keycloak/keycloak
   mvn clean install -Pdistribution -DskipTests -f keycloak -B
   find keycloak/distribution/server-dist/target -maxdepth 1 -type f -name 'keycloak-[[:digit:]]*.tar.gz' -exec tar xzf {} --strip-components=1 -C keycloak-dist \;
@@ -19,13 +19,13 @@ if [ -n "$PRODUCT" ] && [ "$PRODUCT" == "true" ]; then
   echo "Using RHSSO distribution: $PRODUCT_VERSION"
   "$PRODUCT_DIST/bin/add-user-keycloak.sh" -u admin -p admin
   exit 0
-elif [[ ( -n "$GITHUB_BASE_REF" &&  "$GITHUB_BASE_REF" == "latest" ) ]] || [[ ( -n "$QUICKSTART_BRANCH" && "$QUICKSTART_BRANCH" != "master" ) ]]; then
+elif [[ ( -n "$GITHUB_BASE_REF" &&  "$GITHUB_BASE_REF" == "latest" ) ]] || [[ ( -n "$QUICKSTART_BRANCH" && "$QUICKSTART_BRANCH" != "main" ) ]]; then
     VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" pom.xml)
     echo "Using corresponding Keycloak version: $VERSION"
     latest_release
 else
-  echo "Building Keycloak from upstream/master"
-  upstream_master
+  echo "Building Keycloak from upstream/main"
+  upstream_main
 fi
 
 keycloak-dist/bin/add-user-keycloak.sh -u admin -p admin
