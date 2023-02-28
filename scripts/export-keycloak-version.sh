30daa6a5846879b78a346da3283b772d446ead0f
Jon Koops <jonkoops@gmail.com>
2023-02-28 13:22:41 +0100

    Use a valid SemVer format for the SNAPSHOT version

--- scripts/export-keycloak-version.sh
+++ scripts/export-keycloak-version.sh
@@ -3,5 +3,5 @@
 if [[ ( -n "$GITHUB_BASE_REF" &&  "$GITHUB_BASE_REF" == "latest" ) ]] || [[ ( -n "$QUICKSTART_BRANCH" && "$QUICKSTART_BRANCH" != "main" ) ]]; then
   export KEYCLOAK_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 else
-  export KEYCLOAK_VERSION="999-SNAPSHOT"
+  export KEYCLOAK_VERSION="999.0.0-SNAPSHOT"
 fi
