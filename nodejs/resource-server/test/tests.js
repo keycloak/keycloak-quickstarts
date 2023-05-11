feb57845ed8e2a1bda3cfc50a99e8a3545909e78
Jon Koops <jonkoops@gmail.com>
2023-05-11 12:19:13 +0200

    Replace `tape` with the built-in Node.js test runner

--- nodejs/resource-server/test/tests.js
+++ nodejs/resource-server/test/tests.js
@@ -1,40 +1,41 @@
 import tokenRequester from 'keycloak-request-token';
-import test from 'tape';
+import assert from 'node:assert';
+import test from 'node:test';
 import config from './utils/config.js';
 
-test('Should test public route with no credentials.', async (t) => {
+test('accesses the \'public\' route without credentials.', async () => {
   const response = await fetch('http://localhost:3000/service/public');
   const data = await response.json();
 
-  t.equal(data.message, 'public');
+  assert.strictEqual(data.message, 'public');
 });
 
-test('Should test secured route with no credentials.', async (t) => {
+test('denies the \'secured\' route without credentials.', async () => {
   const response = await fetch('http://localhost:3000/service/secured');
 
-  t.equal(response.statusText, 'Forbidden');
+  assert.strictEqual(response.statusText, 'Forbidden');
 });
 
-test('Should test admin route with no credentials.', async (t) => {
+test('denies the \'admin\' route without credentials.', async () => {
   const response = await fetch('http://localhost:3000/service/admin');
 
-  t.equal(response.statusText, 'Forbidden');
+  assert.strictEqual(response.statusText, 'Forbidden');
 });
 
-test('Should test secured route with user credentials.', async (t) => {
+test('accesses the \'secured\' route with credentials.', async () => {
   const headers = { authorization: `Bearer ${await tokenRequester(config.baseUrl, config.token)}` };
   const response = await fetch('http://localhost:3000/service/secured', { headers });
   const data = await response.json();
 
-  t.equal(data.message, 'secured');
+  assert.strictEqual(data.message, 'secured');
 });
 
-test('Should test secured route with admin credentials.', async (t) => {
+test('accesses the \'admin\' route with credentials.', async () => {
   config.token.username = 'test-admin';
 
   const headers = { authorization: `Bearer ${await tokenRequester(config.baseUrl, config.token)}` };
   const response = await fetch('http://localhost:3000/service/admin', { headers });
   const data = await response.json();
 
-  t.equal(data.message, 'admin');
+  assert.strictEqual(data.message, 'admin');
 });
