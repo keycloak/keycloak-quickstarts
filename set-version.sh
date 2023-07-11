31923f2befdf2340091120ae6e2584d1175dc837
Stian Thorgersen <stianst@gmail.com>
2023-07-11 15:53:58 +0200

    Revert set-version script used by releases (#454)
    Closes #453

---
+++ set-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+NEW_VERSION=$1
+
+mvn versions:set -Dversion.keycloak=$NEW_VERSION -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
+
+sed -i "s|$$VERSION$$|$NEW_VERSION|g" kubernetes/keycloak.yaml
+sed -i "s|$$VERSION$$|$NEW_VERSION|g" openshift/keycloak.yaml
