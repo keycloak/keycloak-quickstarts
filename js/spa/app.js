b7f2a5d2567be5478aab91961a6719f1dcacfe20
Jon Koops <jonkoops@gmail.com>
2024-09-11 09:46:28 +0200

    Use Keycloak JS from NPM instead of server
    Closes #600
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/spa/app.js
+++ js/spa/app.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import stringReplace from 'string-replace-middleware';
 
 const app = express();
 const port = 8080;
 
-app.use(stringReplace({
-  KC_URL: process.env.KC_URL || "http://localhost:8180"
-}));
-
 app.use('/', express.static('public'));
+
+app.use('/vendor/keycloak-js', express.static('node_modules/keycloak-js/dist'));
+app.use('/vendor/jwt-decode', express.static('node_modules/jwt-decode/build/esm'));
+app.use('/vendor/@noble/hashes', express.static('node_modules/@noble/hashes/esm'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
