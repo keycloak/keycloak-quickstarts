716d898577c63f65e340004112284a36c7f28fa0
Peter Skopek <pskopek@redhat.com>
2025-04-01 15:24:18 +0200

    Add new script to update Keycloak Client Libraries version
    Signed-off-by: Peter Skopek <pskopek@redhat.com>

--- set-client-version.sh
+++ set-client-version.sh
@@ -3,5 +3,4 @@
 NEW_VERSION=$1
 
 mvn versions:set-property -Dproperty=version.keycloak.client -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
-
 echo "New Keycloak Client Libraries Version: $NEW_VERSION" >&2
