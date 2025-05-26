bfa7e603689af51e68c91178d484d7954f6fd5fb
mposolda <mposolda@gmail.com>
2025-05-26 11:09:20 +0200

    Updating keycloak.js version to 26.2.0. Fix issues in set-version scripts closes #684
    Signed-off-by: mposolda <mposolda@gmail.com>

--- set-client-version.sh
+++ set-client-version.sh
@@ -2,5 +2,10 @@
 
 NEW_VERSION=$1
 
+if [ "$NEW_VERSION" == "" ]; then
+  echo "Usage: set-client-version.sh <NEW VERSION>"
+  exit 1;
+fi
+
 mvn versions:set-property -Dproperty=version.keycloak.client -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
 echo "New Keycloak Client Libraries Version: $NEW_VERSION" >&2
